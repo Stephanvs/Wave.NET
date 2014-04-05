edfa442cf85da43a05509299c8c015eccd6e39cb
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-04-06 01:12:29 +0200

    Migrated new server project in correct folder

--- Source/WaveNET.Server/Properties/AssemblyInfo.cs
+++ Source/WaveNET.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WaveNet.Server")]
+[assembly: AssemblyTitle("WaveNET.Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WaveNet.Server")]
+[assembly: AssemblyProduct("WaveNET.Server")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
