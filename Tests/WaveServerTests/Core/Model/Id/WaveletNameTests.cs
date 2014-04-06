df84f6b7b259e87733c304c72b6eda8d8b9ce57c
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-04-06 16:42:47 +0200

    Added WaveletName equality test

--- Tests/WaveServerTests/Core/Model/Id/WaveletNameTests.cs
+++ Tests/WaveServerTests/Core/Model/Id/WaveletNameTests.cs
@@ -5,20 +5,46 @@ using WaveNET.Core.Model.Id;
 
 namespace WaveNET.Tests.Core.Model.Id
 {
-    [TestClass]
+    
     public class WaveletNameTests
     {
-        [TestMethod]
-        public void CreatingWaveletNameResultsInCorrectToString()
+        [TestClass]
+        public class WaveletNameSerializationTests
         {
-            var waveId = WaveId.Of("example.com", "w+abcd1234");
-            var waveletId = WaveletId.Of("acmewave.com", "conv+blah");
+            [TestMethod]
+            public void CreatingWaveletNameResultsInCorrectToString()
+            {
+                var waveId = WaveId.Of("example.com", "w+abcd1234");
+                var waveletId = WaveletId.Of("acmewave.com", "conv+blah");
 
-            WaveletName
-                .Of(waveId, waveletId)
-                .ToString()
-                .Should()
-                .Be("[WaveletName example.com/w+abcd1234/acmewave.com/conv+blah]");
+                WaveletName
+                    .Of(waveId, waveletId)
+                    .ToString()
+                    .Should()
+                    .Be("[WaveletName example.com/w+abcd1234/acmewave.com/conv+blah]");
+            }
+        }
+
+        [TestClass]
+        public class WaveletNameEqualityTests
+        {
+            [TestMethod]
+            public void SameDomainAndIdShoudEqual()
+            {
+                var one = CreateWaveletName("example.com", "w+abcd1234");
+                var two = CreateWaveletName("example.com", "w+abcd1234");
+
+                one.Equals(two).Should().BeTrue();
+                two.Equals(one).Should().BeTrue();
+            }
+        }
+
+        public static WaveletName CreateWaveletName(string domain, string id)
+        {
+            var waveId = WaveId.Of(domain, id);
+            var waveletId = WaveletId.Of(domain, id);
+
+            return WaveletName.Of(waveId, waveletId);
         }
     }
 }
