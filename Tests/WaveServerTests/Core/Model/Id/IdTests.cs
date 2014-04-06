b6e40bc70e4d873bc637f924d14cda483d072786
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-04-06 02:24:55 +0200

    Updated gitignore and added some more tests

---
+++ Tests/WaveServerTests/Core/Model/Id/IdTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WaveNET.Core.Model.Id;
+
+namespace WaveNET.Tests.Core.Model.Id
+{
+    public class IdTests
+    {
+        public static WaveId CreateWaveId(string domain, string id)
+        {
+            return new WaveId(domain, id);
+        }
+
+        public static WaveletId CreateWaveletId(string domain, string id)
+        {
+            return new WaveletId(domain, id);
+        }
+
+        [TestClass]
+        public class WaveIdEqualityTests
+        {
+            [TestMethod]
+            public void SameDomainAndIdShoudEqual()
+            {
+                WaveId x = CreateWaveId("domain", "id");
+                WaveId y = CreateWaveId("domain", "id");
+
+                x.Equals(y).Should().BeTrue();
+                y.Equals(x).Should().BeTrue();
+            }
+
+            [TestMethod]
+            public void DifferentDomainSameIdShouldNotEqual()
+            {
+                WaveId x = CreateWaveId("one", "id");
+                WaveId y = CreateWaveId("two", "id");
+
+                x.Equals(y).Should().BeFalse();
+                y.Equals(x).Should().BeFalse();
+            }
+
+            [TestMethod]
+            public void DifferentIdsSameDomainShoudNotEqual()
+            {
+                WaveId x = CreateWaveId("one", "sa");
+                WaveId y = CreateWaveId("one", "id");
+
+                x.Equals(y).Should().BeFalse();
+                y.Equals(x).Should().BeFalse();
+            }
+        }
+
+        [TestClass]
+        public class WaveIdTests
+        {
+            [TestMethod]
+            [ExpectedException(typeof (ArgumentException), AllowDerivedTypes = true)]
+            public void TestNullWaveIdDomainRejected()
+            {
+                WaveId waveId = CreateWaveId(null, "id");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof (ArgumentException), AllowDerivedTypes = true)]
+            public void TestEmptyWaveIdDomainRejected()
+            {
+                WaveId waveId = CreateWaveId("", "id");
+            }
+        }
+
+        [TestClass]
+        public class WaveletIdEqualityTests
+        {
+            [TestMethod]
+            public void SameDomainAndIdShoudEqual()
+            {
+                WaveletId x = CreateWaveletId("domain", "id");
+                WaveletId y = CreateWaveletId("domain", "id");
+
+                x.Equals(y).Should().BeTrue();
+                y.Equals(x).Should().BeTrue();
+            }
+
+            [TestMethod]
+            public void DifferentDomainSameIdShouldNotEqual()
+            {
+                WaveletId x = CreateWaveletId("one", "id");
+                WaveletId y = CreateWaveletId("two", "id");
+
+                x.Equals(y).Should().BeFalse();
+                y.Equals(x).Should().BeFalse();
+            }
+
+            [TestMethod]
+            public void DifferentIdsSameDomainShoudNotEqual()
+            {
+                WaveletId x = CreateWaveletId("one", "sa");
+                WaveletId y = CreateWaveletId("one", "id");
+
+                x.Equals(y).Should().BeFalse();
+                y.Equals(x).Should().BeFalse();
+            }
+        }
+
+        [TestClass]
+        public class WaveletIdTests
+        {
+            [TestMethod]
+            [ExpectedException(typeof (ArgumentException), AllowDerivedTypes = true)]
+            public void TestNullWaveletIdDomainRejected()
+            {
+                WaveletId waveletId = CreateWaveletId(null, "id");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof (ArgumentException), AllowDerivedTypes = true)]
+            public void TestEmptyWaveletIdDomainRejected()
+            {
+                WaveletId waveletId = CreateWaveletId("", "id");
+            }
+        }
+    }
+}
