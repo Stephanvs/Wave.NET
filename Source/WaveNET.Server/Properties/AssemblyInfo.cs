7efa74f8345cee69c23e171d425a9aae1f3ce899
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-04-07 20:46:59 +0200

    Added new server with signalr and helios dependencies

--- Source/WaveNET.Server/Properties/AssemblyInfo.cs
+++ Source/WaveNET.Server/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("187dd732-26ec-4002-b512-5f2f2fc14efc")]
+[assembly: Guid("2b3cb0e4-7fc0-4ce0-9c5e-4cedf5d12a60")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
