730f7dcf3e68c9338f8eddb6828c5ebd5a055479
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-05-03 17:06:40 +0200

    Resharper naming convention

--- Source/WaveNET.Core/Model/Document/Operation/BufferedDocInitialization.cs
+++ Source/WaveNET.Core/Model/Document/Operation/BufferedDocInitialization.cs
@@ -5,72 +5,72 @@ namespace WaveNET.Core.Model.Document.Operation
     public class BufferedDocInitialization
         : AbstractBufferedDocInitialization
     {
-        private readonly IDocOp op;
+        private readonly IDocOp _op;
 
         public BufferedDocInitialization(IDocOp op)
         {
-            this.op = op;
+            this._op = op;
         }
 
         public override int Size()
         {
-            return op.Size();
+            return _op.Size();
         }
 
         public override DocOpComponentType GetType(int i)
         {
-            DocOpComponentType t = op.GetType(i);
+            DocOpComponentType t = _op.GetType(i);
             if (t is DocInitializationComponentType)
             {
                 return t;
             }
-            throw new NotSupportedException("Initialization with unexpected component " + t + ": " + op);
+            throw new NotSupportedException("Initialization with unexpected component " + t + ": " + _op);
         }
 
         public override void ApplyComponent(int i, IDocOpCursor cursor)
         {
-            op.ApplyComponent(i, InitializationCursorAdapter.Adapt(cursor));
+            _op.ApplyComponent(i, InitializationCursorAdapter.Adapt(cursor));
         }
 
         public override IAnnotationBoundaryMap GetAnnotationBoundary(int i)
         {
-            return op.GetAnnotationBoundary(i);
+            return _op.GetAnnotationBoundary(i);
         }
 
         public override string GetCharactersString(int i)
         {
-            return op.GetCharactersString(i);
+            return _op.GetCharactersString(i);
         }
 
         public override string GetElementStartTag(int i)
         {
-            return op.GetElementStartTag(i);
+            return _op.GetElementStartTag(i);
         }
 
         public override IAttributes GetElementStartAttributes(int i)
         {
-            return op.GetElementStartAttributes(i);
-        }
+            return _op.GetElementStartAttributes(i);
+        } 
 
         public override void Apply(IDocInitializationCursor cursor)
         {
-            op.Apply(InitializationCursorAdapter.Adapt(cursor));
+            _op.Apply(InitializationCursorAdapter.Adapt(cursor));
         }
 
         public override void ApplyComponent(int i, IDocInitializationCursor cursor)
         {
-            op.ApplyComponent(i, InitializationCursorAdapter.Adapt(cursor));
+            _op.ApplyComponent(i, InitializationCursorAdapter.Adapt(cursor));
         }
 
         public override DocInitializationComponentType GetComponentType(int i)
         {
-            DocOpComponentType t = op.GetType(i);
+            DocOpComponentType t = _op.GetType(i);
             if (t is DocInitializationComponentType)
             {
                 return (DocInitializationComponentType) t;
             }
             throw new InvalidOperationException(
-                "Initialization with unexpected component " + t + ": " + op);
+                "Initialization with unexpected component " + t + ": " + _op);
         }
     }
 }
